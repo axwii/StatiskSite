2d3e93747be62bbb2452765f007f173a5f3514f7
axwii <axwi0001@stud.kea.dk>
2024-02-06 12:40:03 +0100

    Change of naming convention in classes on product

--- js/product.js
+++ js/product.js
@@ -9,19 +9,19 @@ fetch(apiUrl)
 
 function showProduct(product){
     console.log(product);
-    document.querySelector(".purchaseBox .productName").textContent = product.productdisplayname;
-    document.querySelector(".purchaseBox .brand").textContent = product.brandname;
-    document.querySelector(".purchaseBox .price").textContent = product.price;
-    document.querySelector(".purchaseBox .articletype").textContent = product.articletype;
-    document.querySelector(".productImage").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
-    document.querySelector(".productImage").alt = product.productdisplayname;
-    document.querySelector(".breadcrumbsProductName").textContent = product.productdisplayname;
+    document.querySelector(".purchase_box .product_name").textContent = product.productdisplayname;
+    document.querySelector(".purchase_box .brand").textContent = product.brandname;
+    document.querySelector(".purchase_box .price").textContent = product.price;
+    document.querySelector(".purchase_box .article_type").textContent = product.articletype;
+    document.querySelector(".product_image").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+    document.querySelector(".product_image").alt = product.productdisplayname;
+    document.querySelector(".breadcrumbs_product_name").textContent = product.productdisplayname;
     // Info
-    document.querySelector(".info .variantName").textContent = product.variantname;
-    document.querySelector(".info .baseColour").textContent = product.basecolour;
-    document.querySelector(".info .relId").textContent = product.relid;
-    document.querySelector(".info .brandName").textContent = product.brandname;
-    document.querySelector(".info .brandBio").textContent = product.brandbio;
-    document.querySelector(".info .brandImage").src = product.brandimage;
-    document.querySelector(".info .brandImage").alt = product.brandname;
+    document.querySelector(".info .variant_name").textContent = product.variantname;
+    document.querySelector(".info .base_colour").textContent = product.basecolour;
+    document.querySelector(".info .rel_id").textContent = product.relid;
+    document.querySelector(".info .brand_name").textContent = product.brandname;
+    document.querySelector(".info .brand_bio").textContent = product.brandbio;
+    document.querySelector(".info .brand_image").src = product.brandimage;
+    document.querySelector(".info .brand_image").alt = product.brandname;
 }
